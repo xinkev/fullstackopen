938c8cc33089d474d76fe39803f57a113a2fec96
xinkev <lyx13710@gmail.com>
2023-03-27 22:43:49 +0630

    Add 2.2

--- part2/courseinfo/src/components/Course.js
+++ part2/courseinfo/src/components/Course.js
@@ -1,29 +1,33 @@
 const Header = ({ course }) => <h1>{course.name}</h1>
 
-const Part = ({ part }) => 
-  <p>
-    {part.name} {part.exercises}
-  </p>
+const Part = ({ part }) =>
+    <p>
+        {part.name} {part.exercises}
+    </p>
 
-const Content = ({ parts }) => 
-  <>
-    <Part
-      part={parts[0]} 
-    />
-    <Part
-      part={parts[1]} 
-    />
-    <Part
-      part={parts[2]} 
-    />      
-  </>
+const Content = ({ parts }) =>
+    <>
+        <Part
+            part={parts[0]}
+        />
+        <Part
+            part={parts[1]}
+        />
+        <Part
+            part={parts[2]}
+        />
+    </>
+
+const Total = ({ sum }) => <strong>total of {sum} exercises</strong>
 
 const Course = ({ course }) => {
+
     return (
-      <div>
-        <Header course={course} />
-        <Content parts={course.parts} />
-      </div>
+        <div>
+            <Header course={course} />
+            <Content parts={course.parts} />
+            <Total sum={course.parts.reduce((accu, part) => accu + part.exercises, 0)} />
+        </div>
     )
 }
 
