3569ae156673d9c3a782cfc9b9ca4b6759f31d57
xinkev <lyx13710@gmail.com>
2023-04-25 12:13:20 +0630

    Add exercise 2.9

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -3,7 +3,8 @@ import { useState } from 'react'
 const App = () => {
   const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
-  const [number, setNumber] = useState()
+  const [number, setNumber] = useState('')
+  const [filterKeyword, setFilterKeyword] = useState('')
 
   const handleNameChange = (e) => {
     setNewName(e.target.value)
@@ -13,21 +14,29 @@ const App = () => {
     setNumber(e.target.value)
   }
 
+  const handleFilterChange = (e) => {
+    setFilterKeyword(e.target.value)
+  }
+
   const addPerson = (e) => {
     e.preventDefault()
     if (persons.find(person => person.name === newName)) {
       alert(`${newName} is already added to phonebook`)
     } else {
-      const person = { name: newName, number: number}
+      const person = { name: newName, number: number, id: persons.length + 1 }
       setPersons(persons.concat(person))
       setNewName("")
       setNumber("")
+      setFilterKeyword("")
     }
   }
 
   return (
     <div>
       <h2>Phonebook</h2>
+      filter shown with <input value={filterKeyword} onChange={handleFilterChange} />
+
+      <h2>add a new</h2>
       <form onSubmit={addPerson}>
         <div>
           name: <input value={newName} onChange={handleNameChange} />
@@ -40,7 +49,11 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map(person => <div key={person.name}>{person.name} {person.number}</div>)}
+      {persons
+        .filter(person => person && person.name.includes(filterKeyword))
+        .map(person =>
+          <div key={person.id}>{person.name} {person.number}</div>
+        )}
     </div>
   )
 }
