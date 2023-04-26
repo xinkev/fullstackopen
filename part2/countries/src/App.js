73d26d45ec7550814039a5b6766d564681451525
xinkev <lyx13710@gmail.com>
2023-04-26 16:40:09 +0630

    Fix filtering logic error

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -22,7 +22,7 @@ const App = () => {
 
   const filteredCountries = () => {
     return countries.filter((country) =>
-      country.name.common.toLowerCase().includes(query)
+      country.name.common.toLowerCase().includes(query.toLowerCase())
     )
   }
 
