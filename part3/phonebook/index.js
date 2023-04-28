ff92c81958043e22e043f5fede78df92751539fb
xinkev <lyx13710@gmail.com>
2023-04-28 16:39:50 +0630

    Exercise 3.4

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,7 +1,7 @@
 const express = require("express")
 const app = express()
 
-const persons = [
+var persons = [
   {
     id: 1,
     name: "Arto Hellas",
@@ -38,12 +38,19 @@ app.get("/api/persons", (request, response) => {
 
 app.get("/api/persons/:id", (request, response) => {
   const id = Number(request.params.id)
-  const person = persons.find(person => person.id === id)
+  const person = persons.find((person) => person.id === id)
   if (person) {
     response.json(person)
   } else {
     response.status(404).end()
   }
+})
+
+app.delete("/api/persons/:id", (request, response) => {
+  const id = Number(request.params.id)
+  persons = persons.filter((person) => person.id !== id)
+
+  response.status(204).end()
 })
 
 const PORT = 3001
