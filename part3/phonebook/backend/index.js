3a720a2c8497bb9d42c5bd9d4be4dc3b89fee68a
xinkev <lyx13710@gmail.com>
2023-05-01 10:00:35 +0630

    Exercise 3.13

--- part3/phonebook/backend/index.js
+++ part3/phonebook/backend/index.js
@@ -1,8 +1,10 @@
+require("dotenv").config()
 const express = require("express")
 const app = express()
 const morgan = require("morgan")
 const cors = require("cors")
 const path = require("path")
+const Person = require("./models/person")
 
 morgan.token("payload", function (req, res) {
   if (req.method == "POST") {
@@ -21,49 +23,31 @@ app.use(
   )
 )
 
-var persons = [
-  {
-    id: 1,
-    name: "Arto Hellas",
-    number: "040-123456",
-  },
-  {
-    id: 2,
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-  },
-  {
-    id: 3,
-    name: "Dan Abramov",
-    number: "12-43-234345",
-  },
-  {
-    id: 4,
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-  },
-]
-
 app.get("/info", (request, response) => {
-  const html = `
+  Person.find({}).then((persons) => {
+    const html = `
     <p>Phonebook has info for ${persons.length} people</p>
     <p>${Date()}</p>
   `
-  response.send(html)
+    response.send(html)
+  })
 })
 
 app.get("/api/persons", (request, response) => {
-  response.json(persons)
+  Person.find({}).then((persons) => {
+    response.json(persons)
+  })
 })
 
 app.get("/api/persons/:id", (request, response) => {
-  const id = Number(request.params.id)
-  const person = persons.find((person) => person.id === id)
-  if (person) {
-    response.json(person)
-  } else {
-    response.status(404).end()
-  }
+  Person.findById(request.params.id)
+  .then(person => {
+    if (person) {
+      response.json(person)
+    } else {
+      response.status(404).end()
+    }
+  })
 })
 
 app.delete("/api/persons/:id", (request, response) => {
---
+++ part3/phonebook/backend/models/person.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose")
+
+mongoose.set("strictQuery", false)
+
+const url = process.env.MONGODB_URI
+
+console.log("connecting to", url)
+
+mongoose
+  .connect(url)
+  .then((resul) => {
+    console.log("connected to MongoDB")
+  })
+  .catch((error) => {
+    console.log("error connecting to MongoDB", error.message)
+  })
+
+const schema = mongoose.Schema({ name: String, number: String })
+
+schema.set("toJSON", {
+    transform: (document, returnedObject) => {
+        returnedObject.id = returnedObject._id.toString()
+        delete returnedObject._id
+        delete returnedObject.__v
+    }
+})
+
+module.exports = mongoose.model("Person", schema)
