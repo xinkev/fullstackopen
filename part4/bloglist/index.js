b9ab5ec61b620c8c9770307db49ecf6f0e23bc77
xinkev <lyx13710@gmail.com>
2023-05-03 19:38:01 +0630

    Exercise 4.2: Add some loggings

--- part4/bloglist/index.js
+++ part4/bloglist/index.js
@@ -7,7 +7,15 @@ const logger = require("./utils/logger")
 const blogRouter = require("./controllers/blogs")
 const middleware = require("./utils/middleware")
 
+logger.info("connecting to", config.MONGODB_URI)
+
 mongoose.connect(config.MONGODB_URI)
+  .then(() => {
+    logger.info("connected to MongoDB")
+  })
+  .catch((error) => {
+    logger.error("error connecting to MongoDB", error.message)
+  })
 
 app.use(cors())
 app.use(express.json())
