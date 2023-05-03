6eee205c8c9663df98b2aef3064d725741eb6abb
xinkev <lyx13710@gmail.com>
2023-05-03 19:22:50 +0630

    Exercise 4.2 - Refactor: Add logger module

--- part4/bloglist/index.js
+++ part4/bloglist/index.js
@@ -3,6 +3,7 @@ const express = require("express")
 const app = express()
 const cors = require("cors")
 const mongoose = require("mongoose")
+const logger = require("./utils/logger")
 const Blog = require("./models/blog")
 
 const mongoUri = process.env.MONGODB_URI
@@ -31,5 +32,5 @@ app.post("/api/blogs", (request, response) => {
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+  logger.info(`Server running on port ${PORT}`)
 })
