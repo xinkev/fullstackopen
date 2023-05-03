8df08282057c3b168587e6cfee3ec92f63d883ae
xinkev <lyx13710@gmail.com>
2023-05-03 19:31:08 +0630

    Exercise 4.2 - Refactor: Extract Blog routes into its own module

--- part4/bloglist/index.js
+++ part4/bloglist/index.js
@@ -4,30 +4,14 @@ const app = express()
 const cors = require("cors")
 const mongoose = require("mongoose")
 const logger = require("./utils/logger")
-const Blog = require("./models/blog")
+const blogRouter = require("./controllers/blogs")
 
 mongoose.connect(config.MONGODB_URI)
 
 app.use(cors())
 app.use(express.json())
 
-app.get("/api/blogs", (request, response) => {
-  Blog.find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post("/api/blogs", (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
-
+app.use("/api/blogs", blogRouter)
 
 app.listen(config.PORT, () => {
   logger.info(`Server running on port ${config.PORT}`)
