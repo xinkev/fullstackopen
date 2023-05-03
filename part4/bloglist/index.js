b5ecd36d68fa31ddf2ea973b10d27bb20f7f38f8
xinkev <lyx13710@gmail.com>
2023-05-03 19:35:07 +0630

    Exercise 4.2: Add middlewares

--- part4/bloglist/index.js
+++ part4/bloglist/index.js
@@ -5,13 +5,18 @@ const cors = require("cors")
 const mongoose = require("mongoose")
 const logger = require("./utils/logger")
 const blogRouter = require("./controllers/blogs")
+const middleware = require("./utils/middleware")
 
 mongoose.connect(config.MONGODB_URI)
 
 app.use(cors())
 app.use(express.json())
+app.use(middleware.requestLogger)
 
 app.use("/api/blogs", blogRouter)
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
 
 app.listen(config.PORT, () => {
   logger.info(`Server running on port ${config.PORT}`)
