1f4d009176a4baea7cc0a4b9851d7ef19f884599
xinkev <lyx13710@gmail.com>
2023-05-03 20:09:17 +0630

    Exercise 4.4

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -2,6 +2,15 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+  if (blogs.length <= 0) {
+    return 0
+  } else {
+    return blogs.reduce((acc, blog) => acc + blog.likes, 0)
+  }
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
