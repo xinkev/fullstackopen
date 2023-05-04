81f407958b1e74b817f6a7eb63f0e586a974eeac
xinkev <lyx13710@gmail.com>
2023-05-04 15:03:21 +0630

    Add lodash library and replace the logic of finding the blog with most likes withmaxBy from lodash

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -1,16 +1,32 @@
+const maxBy = require("lodash/maxBY")
+
 const dummy = (blogs) => {
   return 1
 }
 
 const totalLikes = (blogs) => {
-  if (blogs.length <= 0) {
+  if (blogs.length === 0) {
     return 0
   } else {
     return blogs.reduce((acc, blog) => acc + blog.likes, 0)
   }
 }
 
+const favoriteBlog = (blogs) => {
+  if (!blogs || blogs.length === 0) {
+    return undefined
+  } else {
+    const blog = maxBy(blogs, (blog) => blog.likes)
+    return {
+      title: blog.title,
+      author: blog.author,
+      likes: blog.likes,
+    }
+  }
+}
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog,
 }
