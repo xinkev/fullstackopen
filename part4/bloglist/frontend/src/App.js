bdaac8e1525d2f356a06db6012477979cfff034d
xinkev <lyx13710@gmail.com>
2023-05-14 22:27:10 +0630

    Exercise 5.2

--- part4/bloglist/frontend/src/App.js
+++ part4/bloglist/frontend/src/App.js
@@ -4,6 +4,7 @@ import blogService from "./services/blogs"
 import loginService from "./services/login"
 
 const App = () => {
+  const LOCAL_KEY_USER = "loggedin_user"
   const [blogs, setBlogs] = useState([])
   const [user, setUser] = useState()
   const [username, setUsername] = useState("")
@@ -16,21 +17,34 @@ const App = () => {
     }
   }, [user])
 
+  useEffect(() => {
+    const userString = window.localStorage.getItem(LOCAL_KEY_USER)
+    setUser(JSON.parse(userString))
+  }, [])
+
   const onLoginSubmit = async (e) => {
     e.preventDefault()
     try {
       const user = await loginService.login({ username, password })
+      window.localStorage.setItem(LOCAL_KEY_USER, JSON.stringify(user))
       setUser(user)
     } catch (exception) {
       console.log("failed to login", exception)
     }
   }
 
+  const onLogoutClick = (e) => {
+    window.localStorage.removeItem(LOCAL_KEY_USER)
+    setUser(null)
+  }
+
   if (user) {
     return (
       <div>
         <h2>blogs</h2>
-        <p>{user.name} logged in</p>
+        <div>
+          {user.name} logged in <button onClick={onLogoutClick}>logout</button>
+        </div>
         {blogs.map((blog) => (
           <Blog key={blog.id} blog={blog} />
         ))}
