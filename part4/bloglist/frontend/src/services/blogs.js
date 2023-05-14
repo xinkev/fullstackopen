ecbe6ff2156f7e5142bf0b233d964fb9a960e2c5
xinkev <lyx13710@gmail.com>
2023-05-14 22:56:21 +0630

    Add token to default headers

--- part4/bloglist/frontend/src/services/blogs.js
+++ part4/bloglist/frontend/src/services/blogs.js
@@ -1,15 +1,12 @@
 import axios from "axios"
 const baseUrl = "/api/blogs"
 
-let token = null
-
 const setToken = (newToken) => {
-  token = "Bearer " + newToken
+  axios.defaults.headers.common["Authorization"] = "Bearer " + newToken
 }
 
 const getAll = () => {
-  const config = { headers: { Authorization: token } }
-  const request = axios.get(baseUrl, config)
+  const request = axios.get(baseUrl)
   return request.then((response) => response.data)
 }
 
