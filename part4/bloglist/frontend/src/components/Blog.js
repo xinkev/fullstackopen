1a20d74989101189f4c3bdf03090083822aac5d2
xinkev <lyx13710@gmail.com>
2023-05-16 18:48:13 +0630

    5.7

--- part4/bloglist/frontend/src/components/Blog.js
+++ part4/bloglist/frontend/src/components/Blog.js
@@ -1,7 +1,33 @@
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react"
+
+const Blog = ({ blog }) => {
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: "solid",
+    borderWidth: 1,
+    marginBottom: 5,
+  }
+
+  const [isVisible, setIsVisible] = useState(false)
+
+  return (
+    <div style={blogStyle}>
+      <div>
+        {blog.title} {blog.author}
+        <button onClick={() => setIsVisible(!isVisible)}>
+          {isVisible ? "hide" : "view"}
+        </button>
+      </div>
+      <div style={{ display: isVisible ? "" : "none" }}>
+        <div>{blog.url}</div>
+        <div>
+          likes {blog.likes} <button>like</button>
+        </div>
+        <div>{blog.user && blog.user.name}</div>
+      </div>
+    </div>
+  )
+}
 
 export default Blog
