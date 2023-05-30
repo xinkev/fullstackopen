6b037a9e69dcd2c31fe1e44d65d50d96465dfc24
xinkev <lyx13710@gmail.com>
2023-05-31 00:30:08 +0630

    8.10: Adding a book

--- part8/library-frontend/src/components/NewBook.js
+++ part8/library-frontend/src/components/NewBook.js
@@ -1,4 +1,6 @@
 import { useState } from 'react'
+import { useMutation } from '@apollo/client'
+import { ALL_AUTHORS, ALL_BOOKS, CREATE_BOOK } from '../queries'
 
 const NewBook = (props) => {
   const [title, setTitle] = useState('')
@@ -6,6 +8,9 @@ const NewBook = (props) => {
   const [published, setPublished] = useState('')
   const [genre, setGenre] = useState('')
   const [genres, setGenres] = useState([])
+
+  const [createBook] = useMutation(CREATE_BOOK,
+    { refetchQueries: [{ query: ALL_BOOKS }, { query: ALL_AUTHORS }] })
 
   if (!props.show) {
     return null
@@ -15,6 +20,9 @@ const NewBook = (props) => {
     event.preventDefault()
 
     console.log('add book...')
+
+    createBook(
+      { variables: { title, author, published: Number(published), genres } })
 
     setTitle('')
     setPublished('')
